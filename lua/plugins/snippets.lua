a3eeaf2184abb5789f6514839a9e6d19fafdcda9
tiaswidiatmika <tiaswidiatmika@gmail.com>
2025-08-28 01:45:50 +0700

    fork from omerxx

---
+++ lua/plugins/snippets.lua
@@ -0,0 +1,10 @@
+-- ~/.config/nvim/lua/plugins/snippets.lua
+return {
+    {
+        "rafamadriz/friendly-snippets",
+        config = function()
+            require("luasnip").filetype_extend("javascriptreact", { "html" })
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end,
+    },
+}
