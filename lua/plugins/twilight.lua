41e3f7530c7007595b42d28df90ca9eac6bc78c6
tiaswidiatmika <tiaswidiatmika@gmail.com>
2025-08-28 02:21:19 +0700

    install twilight

---
+++ lua/plugins/twilight.lua
@@ -0,0 +1,9 @@
+return {
+    "folke/twilight.nvim",
+    keys = {
+        { "<leader>tw", "<cmd>Twilight<cr>", desc = "Toggle Twilight" },
+    },
+    opts = {
+        -- your configuration
+    },
+}
