47bd752545fc64297360b8b9dcbb2824c1eef8cb
Tias Widiatmika <tiaswidiatmika@gmail.com>
2025-10-03 02:14:41 +0700

    add several formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,7 +2,13 @@ return {
     "stevearc/conform.nvim",
     opts = {
         formatters_by_ft = {
-            yaml = { "yamlfmt" }, -- Replace default with K8s-friendly formatter
+            yaml = { "yamlfmt" }, -- Your existing YAML config
+            vue = { "prettier" }, -- Add Vue formatting
+            html = { "prettier" }, -- Add HTML formatting
+            css = { "prettier" }, -- Add CSS formatting
+            scss = { "prettier" }, -- Add SCSS formatting
+            javascript = { "prettier" }, -- Add JS formatting
+            typescript = { "prettier" }, -- Add TS formatting
         },
         formatters = {
             yamlfmt = {
