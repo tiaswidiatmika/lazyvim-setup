177c6ec564023bf3708f296598d35ac0a4f80d36
Tias Widiatmika <tiaswidiatmika@gmail.com>
2025-10-03 02:07:41 +0700

    add line mover using option key

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,22 @@
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
 vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
+vim.api.nvim_set_keymap("n", "<leader>ww", ":w<CR>", {
+    noremap = true,
+    silent = true,
+    desc = "Save file",
+})
+
+vim.api.nvim_set_keymap("n", "<leader>qq", ":q<CR>", {
+    noremap = true,
+    silent = true,
+    desc = "Quit Window",
+})
+
+-- line movement keymaps
+vim.api.nvim_set_keymap("n", "<A-j>", ":m .+1<CR>==", { noremap = true, silent = true, desc = "Move line down" })
+vim.api.nvim_set_keymap("n", "<A-k>", ":m .-2<CR>==", { noremap = true, silent = true, desc = "Move line up" })
+vim.api.nvim_set_keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true, desc = "Move lines down" })
+vim.api.nvim_set_keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true, desc = "Move lines up" })
+vim.api.nvim_set_keymap("i", "<A-j>", "<Esc>:m .+1<CR>==gi", { noremap = true, silent = true, desc = "Move line down" })
+vim.api.nvim_set_keymap("i", "<A-k>", "<Esc>:m .-2<CR>==gi", { noremap = true, silent = true, desc = "Move line up" })
